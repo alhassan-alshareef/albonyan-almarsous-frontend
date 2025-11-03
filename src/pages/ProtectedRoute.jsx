94b1aee5c9c1b4698aa25b92fed879b03f8bb749
Alhassanalshareff <alhassan.alshareff@gmail.com>
2025-11-03 05:18:52 +0300

    add role-based route protection in ProtectedRoute

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -3,7 +3,20 @@ import { Navigate } from "react-router-dom";
 import { getUserFromToken } from "../lib/auth";
 
 export default function ProtectedRoute({ children }) {
-    const user = getUserFromToken();
-    if (!user) return <Navigate to="/login" replace />;
-        return children;
+  const user = getUserFromToken();
+  const location = useLocation();
+
+  if (!user) return <Navigate to="/login" replace />;
+
+
+  if (location.pathname.startsWith("/patient") && user.role !== "patient") {
+    return <Navigate to="/posts" replace />;
+  }
+
+  if ((location.pathname.startsWith("/posts") || location.pathname.startsWith("/donations")) 
+      && user.role !== "supporter") {
+    return <Navigate to="/patient/posts" replace />;
+  }
+
+  return children;
 }
